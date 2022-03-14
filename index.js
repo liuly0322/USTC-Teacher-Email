99bf43a64c95444a347389541ac39c46448de750
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-03-14 17:43:19 +0800

    style: ajax default async is true

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ async function getTeacherEmail(teacher, terms, interval) {
 
   // 首先获取学生查询的标识码
   const queryDoc = await $.ajax({
-    async: true,
     url: "https://jw.ustc.edu.cn/for-std/lesson-search",
   });
   const student_id = queryDoc.match(
@@ -83,10 +82,7 @@ async function getTeacherEmail(teacher, terms, interval) {
   async function getCourseID() {
     for (let i = 0; i < terms; i++) {
       const course_url = `https://jw.ustc.edu.cn/for-std/lesson-search/semester/${POOL[i]}/search/${student_id}?teacherNameLike=${teacher}`;
-      const course_info = await $.ajax({
-        async: true,
-        url: course_url,
-      });
+      const course_info = await $.ajax({ url: course_url });
       if (course_info.data.length) {
         return course_info.data[0].id;
       } else {
@@ -99,10 +95,7 @@ async function getTeacherEmail(teacher, terms, interval) {
   // 根据课程 id，查询邮箱
   const course_id = await getCourseID();
   const email_url = `https://jw.ustc.edu.cn/ws/course-adjustment-apply/get-teacher-info?lessonId=${course_id}`;
-  const email_res = await $.ajax({
-    async: true,
-    url: email_url,
-  });
+  const email_res = await $.ajax({ url: email_url });
 
   email_res.forEach((object) => {
     if (object[teacher]) {
