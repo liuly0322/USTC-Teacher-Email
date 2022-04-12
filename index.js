93e5261c047137aedd13973bcb36e297d2135095
Liu <liuly0322@mail.ustc.edu.cn>
2022-04-12 19:02:10 +0800

    style: async/await & perf

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 async function getTeacherEmail(teacher, terms, interval) {
   // 程序配置（不要随意修改，除非你知道你在做什么）
   const POOL = [
-    "281",
-    "261",
     "241",
     "221",
     "202",
     "201",
+    "281",
+    "261",
     "181",
     "161",
     "141",
@@ -79,7 +79,7 @@ async function getTeacherEmail(teacher, terms, interval) {
   )[1];
 
   // 查找老师教的课程 id
-  async function getCourseID() {
+  const course_id = await (async () => {
     for (let i = 0; i < terms; i++) {
       const course_url = `https://jw.ustc.edu.cn/for-std/lesson-search/semester/${POOL[i]}/search/${student_id}?teacherNameLike=${teacher}`;
       const course_info = await $.ajax({ url: course_url });
@@ -90,10 +90,9 @@ async function getTeacherEmail(teacher, terms, interval) {
         await new Promise((r) => setTimeout(r, interval * 1000));
       }
     }
-  }
+  })();
 
   // 根据课程 id，查询邮箱
-  const course_id = await getCourseID();
   const email_url = `https://jw.ustc.edu.cn/ws/course-adjustment-apply/get-teacher-info?lessonId=${course_id}`;
   const email_res = await $.ajax({ url: email_url });
 
